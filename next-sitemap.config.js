0d141107454b49fa882008de952fd9693907ee2a
sert121 <yashrajmore29@gmail.com>
2024-10-22 00:58:01 -0400

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sert121.github.io', // Replace with your GitHub Pages URL
+    generateRobotsTxt: true, // Optionally generate robots.txt
+    outDir: 'out', // Ensure the sitemap is generated in the static output folder
+  };
+  
